9e23a214ab98272e5381bb83cbff1dffbe834c75
rinakarnauch <rina.karnauch@mail.huji.ac.il>
2023-01-18 15:22:03 +0200

    fixed 5

--- ex6.py
+++ ex6.py
@@ -91,6 +91,7 @@ class BayesianGMM:
         self._inv_sig = 1 / self._sig_prior
 
         # for gibbs sampling
+        self._N_k = np.zeros(shape=self._k)
         self._pi = np.random.dirichlet(alpha=np.array([self._alpha for k in range(self._k)]))
         self._mu_ks = np.random.multivariate_normal(mean=self._mu_prior,
                                                     cov=self._I_d * self._sig_prior,
@@ -103,15 +104,17 @@ class BayesianGMM:
         :param X: the data points whose log-likelihood should be calculated, as a numpy array of shape [N, d]
         :return: the log-likelihood of each point under each Gaussian in the GMM
         """
-        if self._learn_cov:
-            log_likelihood = np.array(
-                [multivariate_normal.logpdf(X, mean=self._mu_ks[v_k], cov=self._sig_ks[v_k]) for v_k in range(self._k)])
-        else:
-            common_value = self._d * np.log(2 * np.pi * self._beta)
-            x_k = np.array([X - mu_k for mu_k in self._mu_ks])
-            inner = np.array([np.linalg.norm(x, axis=1) ** 2 for x in x_k])
-            log_likelihood = -.5 * (
-                        common_value + self._inv_beta * inner)  # L[j,i] : log likelihood of cluster j of point i
+
+        # ----
+        dist = np.sum(X*X, axis=1)[None, :] - 2*self._mu_ks @ X.T + np.sum(self._mu_ks*self._mu_ks, axis=1)[:, None]
+        log_likelihood_1 = -.5 * (self._d * np.log(2*np.pi*self._beta) + (self._inv_beta) * dist)
+        log_likelihood = np.log(self._pi)[:, None] + log_likelihood_1
+        # ----
+        # if self._learn_cov: log_likelihood = np.array( [multivariate_normal.logpdf(X, mean=self._mu_ks[v_k],
+        # cov=self._sig_ks[v_k]) for v_k in range(self._k)]) else: common_value = self._d * np.log(2 * np.pi *
+        # self._beta) x_k = np.array([X - mu_k for mu_k in self._mu_ks]) inner = np.array([np.linalg.norm(x,
+        # axis=1) ** 2 for x in x_k]) log_likelihood = -.5 * ( common_value + self._inv_beta * inner) # L[j,
+        # i] : log likelihood of cluster j of point i
         return log_likelihood
 
     def cluster(self, X: np.ndarray) -> np.ndarray:
@@ -125,21 +128,29 @@ class BayesianGMM:
         clusters = np.argmax(log_likelihood, axis=0)
         return clusters
 
-    def get_mu_ks(self, cov_k):
-        if self._learn_cov:
-            inv_sig = np.array([np.linalg.inv(cov_k[v_k]) for v_k in range(self._k)])
-            cov = np.array([self._N_k[v_k] * inv_sig[v_k] + self._sig_prior * self._I_d for v_k in range(self._k)])
-            cov = np.array([np.linalg.inv(m) for m in cov])
-            mu = np.array(
-                [cov[v_k] @ ((inv_sig[v_k] @ self._sum_X_per_k[v_k]) + (self._inv_sig * self._mu_prior)) for v_k in
-                 range(self._k)])
-        else:
-            denominator_arr = np.array([1 / (n_k * self._inv_beta + self._inv_sig) for n_k in self._N_k])
-            cov = np.array([self._I_d * denom for denom in denominator_arr])
-            mu = self._inv_beta * self._sum_X_per_k + (self._inv_sig * self._mu_prior)
-            mu = np.array([mu[i] / denominator_arr[i] for i in range(self._k)])
-        return np.array([np.random.multivariate_normal(mu[v_k], cov[v_k]) for v_k in range(self._k)])
+    # def get_mu_ks(self, cov_k):
+    #     if self._learn_cov:
+    #         inv_sig = np.array([np.linalg.inv(cov_k[v_k]) for v_k in range(self._k)])
+    #         cov = np.array([self._N_k[v_k] * inv_sig[v_k] + self._sig_prior * self._I_d for v_k in range(self._k)])
+    #         cov = np.array([np.linalg.inv(m) for m in cov])
+    #         mu = np.array(
+    #             [cov[v_k] @ ((inv_sig[v_k] @ self._sum_X_per_k[v_k]) + (self._inv_sig * self._mu_prior)) for v_k in
+    #              range(self._k)])
+    #     else:
+    #         denominator_arr = np.array([1 / (n_k * self._inv_beta + self._inv_sig) for n_k in self._N_k])
+    #         cov = np.array([self._I_d * denom for denom in denominator_arr])
+    #         mu = self._inv_beta * self._sum_X_per_k + (self._inv_sig * self._mu_prior)
+    #         mu = np.array([mu[i] / denominator_arr[i] for i in range(self._k)])
+    #     return np.array([np.random.multivariate_normal(mu[v_k], cov[v_k]) for v_k in range(self._k)])
 
+    def get_dist(self,X, mu, indices):
+        x_indices = X[indices]
+        if np.size(x_indices) == 0:
+            return np.zeros(shape=self._I_d.shape)
+        else:
+            sub = x_indices - mu[None, :]
+            s = sub.T @ sub
+            return s
     def gibbs_fit(self, X: np.ndarray, T: int) -> 'BayesianGMM':
         """
         Fits the Bayesian GMM model using a Gibbs sampling algorithm
@@ -148,28 +159,44 @@ class BayesianGMM:
         :return: the fitted model
         """
         self._N = X.shape[0]
-
         for t in range(T):
             self._L = self.log_likelihood(X)
-            self._q = np.exp(self._L - logsumexp(self._L, axis=0)[None, :])
-            self._z_t = np.random.default_rng().multinomial(1, self._q.T, size=self._N).argmax(axis=-1)
-            indices_per_k = [np.where(self._z_t == v_k) for v_k in range(self._k)]
-            self._sum_X_per_k = np.array([np.sum(X[indices], axis=0) for indices in indices_per_k])
-            self._N_k = np.array([np.count_nonzero(self._z_t == v_k) for v_k in range(self._k)])
+            self._q = np.exp(self._L - logsumexp(self._L, axis=0)[None, :]).T
+            self._z_t = np.random.default_rng().multinomial(1, self._q, size=self._N).argmax(axis=-1)
+            self._N_k = np.bincount(self._z_t, minlength=self._k)
             self._pi = np.random.dirichlet(alpha=self._alpha + self._N_k)
-            # self._sig_ks = (self._nu * self._I_d * self._beta + sub_inner @ sub_inner.T) / self._nu + self._N_k
-            if self._learn_cov:
-                sig_common = self._nu + self._I_d * self._beta
-                sig_array_denominator = self._nu + self._N_k
-                inner_array = [X[indices] for indices in indices_per_k]
-                inner_array_minus_mu_ks = [inner_array[v_k] - self._mu_ks[v_k] for v_k in range(self._k)]
-                inner_value = [np.zeros(self._d) if i_v_k.shape[0] == 0 else i_v_k.T @ i_v_k for i_v_k in
-                               inner_array_minus_mu_ks]
-                self._sig_ks = np.array(
-                    [(sig_common + inner_value[v_k]) / sig_array_denominator[v_k] for v_k
-                     in range(self._k)])
-            self._mu_ks = self.get_mu_ks(self._sig_ks)
+            for v_k in range(self._k):
+                indices_k = np.where(self._z_t == v_k)[0]
+                x_k = np.sum(X[indices_k], axis=0)
+                if self._learn_cov:
+                    dist = self.get_dist(X, self._mu_ks[v_k], indices_k)
+                    self._sig_ks[v_k] = ((self._nu * self._beta * self._I_d) + dist) / (self._nu + self._N_k[v_k])
+                inv_cov = np.linalg.inv(self._sig_ks[v_k])
+                mu_cov = np.linalg.inv(self._N_k[v_k] * inv_cov + self._inv_sig * self._I_d)
+                m_k = mu_cov @ (inv_cov @ x_k + self._inv_sig * self._mu_prior)
+                self._mu_ks[v_k] = np.random.multivariate_normal(m_k, mu_cov)
         return self
+        #
+        # for t in range(T):
+        #     self._L = self.log_likelihood(X)
+        #     self._q = np.exp(self._L - logsumexp(self._L, axis=0)[None, :])
+        #     self._z_t = np.random.default_rng().multinomial(1, self._q.T, size=self._N).argmax(axis=-1)
+        #     indices_per_k = [np.where(self._z_t == v_k) for v_k in range(self._k)]
+        #     self._sum_X_per_k = np.array([np.sum(X[indices], axis=0) for indices in indices_per_k])
+        #     self._N_k = np.array([np.count_nonzero(self._z_t == v_k) for v_k in range(self._k)])
+        #     self._pi = np.random.dirichlet(alpha=self._alpha + self._N_k)
+        #     # self._sig_ks = (self._nu * self._I_d * self._beta + sub_inner @ sub_inner.T) / self._nu + self._N_k
+        #     if self._learn_cov:
+        #         sig_common = self._nu + self._I_d * self._beta
+        #         sig_array_denominator = self._nu + self._N_k
+        #         inner_array = [X[indices] for indices in indices_per_k]
+        #         inner_array_minus_mu_ks = [inner_array[v_k] - self._mu_ks[v_k] for v_k in range(self._k)]
+        #         inner_value = [np.zeros(self._d) if i_v_k.shape[0] == 0 else i_v_k.T @ i_v_k for i_v_k in
+        #                        inner_array_minus_mu_ks]
+        #         self._sig_ks = np.array(
+        #             [(sig_common + inner_value[v_k]) / sig_array_denominator[v_k] for v_k
+        #              in range(self._k)])
+        #     self._mu_ks = self.get_mu_ks(self._sig_ks)
 
 
 if __name__ == '__main__':
@@ -218,11 +245,11 @@ if __name__ == '__main__':
         plt.bar(np.arange(len(pi)), np.sort(pi)[::-1])
         plt.ylabel(r'$\pi_k$')
         plt.xlabel('cluster number')
+        plt.title(f'histogram #{i}')
         plt.show()
 
         # plot the fitted 2D GMM
-        plot_2D_gmm(X, gmm._mu_ks, gmm._sig_ks,
-                    gmm.cluster(X))  # the second input are the means and the third are the covariances
+        plot_2D_gmm(X, gmm._mu_ks, gmm._sig_ks, gmm.cluster(X))  # the second input are the means and the third are the covariances
 
     # # ---------------------- questions 6-7
     # # load image data
